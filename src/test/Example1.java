3ccc6c8e083c15ab6e459a9274c8e69e559cffa5
Gaurav <gaurav.gulhane@gmail.com>
2019-11-13 14:58:36 +0530

    First code Commit

---
+++ src/test/Example1.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
