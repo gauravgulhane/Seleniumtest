63ee4e0c3ea96648ec7a8b86adf2a4ebc3ffd972
Gaurav <gaurav.gulhane@gmail.com>
2019-11-14 11:45:06 +0530

    Second code commit

--- src/test/Example3.java
+++ src/test/Example3.java
@@ -1,11 +1,10 @@
 package test;
 
-public class Example2 {
+public class Example3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Example2 class");
-		System.out.println("Test GIT");
+		System.out.println("Example3 class");
 	}
 
 }
