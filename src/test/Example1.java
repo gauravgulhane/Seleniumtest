b9162709950ed8b64fffd986a0d7837531b5c96d
Gaurav Gulhane <57661910+gauravgulhane@users.noreply.github.com>
2019-11-15 14:49:39 +0530

    Update Example1.java
    Code changed in github repository direct

--- src/test/Example1.java
+++ src/test/Example1.java
@@ -10,6 +10,7 @@ public class Example1 {
 
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		System.out.println("Example1 - code changed in github repository direct");
 	}
 
 }
